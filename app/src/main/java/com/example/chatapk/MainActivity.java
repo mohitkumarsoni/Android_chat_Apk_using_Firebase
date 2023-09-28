b9d27dc624f309aa1bad825b95c0462c76b75311
mohit soni <mohitkumarsoni135@gmail.com>
2023-09-28 21:14:34 +0530

    main activity fragments with search activity

--- app/src/main/java/com/example/chatapk/MainActivity.java
+++ app/src/main/java/com/example/chatapk/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.chatapk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import org.checkerframework.checker.units.qual.C;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton searchButton;
+    BottomNavigationView bottomNavigationView;
+
+    ChatFragment chatFragment;
+    ProfileFragment profileFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chatFragment = new ChatFragment();
+        profileFragment = new ProfileFragment();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        searchButton = findViewById(R.id.main_search_button);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SearchUserActivity.class));
+            }
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.menu_chat){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, chatFragment).commit();
+                }
+                if (item.getItemId() == R.id.menu_profile){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, profileFragment).commit();
+                }
+                return true;
+            }
+        });
+
+        // fragment selected by default
+        bottomNavigationView.setSelectedItemId(R.id.menu_chat);
+
+
     }
 }
