64eb63e7089be12e135ef17a54d48f6db87b33f4
mohit soni <mohitkumarsoni135@gmail.com>
2023-09-29 23:11:47 +0530

    setup chat activity

--- app/src/main/java/com/example/chatapk/Adapter/Search_User_Recycler_Adapter.java
+++ app/src/main/java/com/example/chatapk/Adapter/Search_User_Recycler_Adapter.java
@@ -1,6 +1,7 @@
 package com.example.chatapk.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +11,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.chatapk.ChatActivity;
 import com.example.chatapk.R;
 import com.example.chatapk.model.UserModel;
+import com.example.chatapk.util.AndroidUtil;
 import com.example.chatapk.util.FireBaseUtil;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
@@ -37,7 +40,10 @@ public class Search_User_Recycler_Adapter extends FirestoreRecyclerAdapter<UserM
 
         holder.itemView.setOnClickListener(v -> {
             // navigate to chat activity
-
+            Intent intent = new Intent(context, ChatActivity.class);
+            AndroidUtil.passUserModelAsIntent(intent, model);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
         });
 
     }
--- app/src/main/java/com/example/chatapk/util/AndroidUtil.java
+++ app/src/main/java/com/example/chatapk/util/AndroidUtil.java
@@ -1,12 +1,28 @@
 package com.example.chatapk.util;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
+
+import com.example.chatapk.model.UserModel;
 
 public class AndroidUtil {
     public static void showToast(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
 
+    public static void passUserModelAsIntent(Intent intent, UserModel userModel){
+        intent.putExtra("username", userModel.getUsername());
+        intent.putExtra("phone", userModel.getPhone());
+        intent.putExtra("userId", userModel.getUserId());
+    }
+
+    public static UserModel getUserModelFromIntent(Intent intent){
+        UserModel userModel = new UserModel();
+        userModel.setUsername(intent.getStringExtra("username"));
+        userModel.setPhone(intent.getStringExtra("phone"));
+        userModel.setUserId(intent.getStringExtra("userId"));
+        return userModel;
+    }
 
 }
