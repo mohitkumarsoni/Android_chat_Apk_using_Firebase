8cac15a5ded5e61c8928853943442c5d0337ebd9
mohit soni <mohitkumarsoni135@gmail.com>
2023-10-04 16:21:18 +0530

    update user Profile and Logout

--- app/src/main/java/com/example/chatapk/ProfileFragment.java
+++ app/src/main/java/com/example/chatapk/ProfileFragment.java
@@ -1,14 +1,37 @@
 package com.example.chatapk;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.chatapk.model.UserModel;
+import com.example.chatapk.util.AndroidUtil;
+import com.example.chatapk.util.FireBaseUtil;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
+    ImageView profilePic;
+    EditText usernameInput;
+    EditText phoneInput;
+    FloatingActionButton updateProfileBtn;
+    ProgressBar progressBar;
+    TextView logoutBtn;
+    UserModel currentUserModel;
 
     public ProfileFragment() {
 
@@ -18,9 +41,79 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
 
+        profilePic = view.findViewById(R.id.profile_image_view);
+        usernameInput = view.findViewById(R.id.profile_username);
+        phoneInput = view.findViewById(R.id.profile_phone);
+        updateProfileBtn = view.findViewById(R.id.profile_update_btn);
+        progressBar = view.findViewById(R.id.profile_progress_bar);
+        logoutBtn = view.findViewById(R.id.profile_logout_btn);
 
+        getUserData();
 
+        updateProfileBtn.setOnClickListener(v -> {
+            updateBtnClicked();
+        });
+
+        logoutBtn.setOnClickListener(v -> {
+            FireBaseUtil.logout();
+            Intent intent = new Intent(getContext(), SplashActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+
+        return view;
+    }
+
+    private void updateBtnClicked() {
+        String newUsername = usernameInput.getText().toString();
+        if (newUsername.isEmpty()|| newUsername.length() < 3 ){
+            usernameInput.setError("minimum username length is 3 characters");
+            return;
+        }
+        currentUserModel.setUsername(newUsername);
+
+        // to remove keyboard from screen as updateBtnClicked
+        try {
+            InputMethodManager inputMethodManager = (InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(Objects.requireNonNull(requireActivity().getCurrentFocus()).getWindowToken(),0);
+        }catch (Exception e){
+            Log.d("PROFILE KEYBOARD ERROR",e.toString());
+        }
+
+        setInProgress(true);
+        updateToFirestore();
+    }
+
+    private void updateToFirestore(){
+        FireBaseUtil.currentUserDetails().set(currentUserModel).addOnCompleteListener(task -> {
+            setInProgress(false);
+            if (task.isSuccessful()){
+                AndroidUtil.showToast(getContext(), "Updated Successfully");
+            }else {
+                AndroidUtil.showToast(getContext(), "Updating Failed");
+            }
+        });
+    }
+
+    private void getUserData() {
+        setInProgress(true);
+        FireBaseUtil.currentUserDetails().get().addOnCompleteListener(task -> {
+            setInProgress(false);
+            currentUserModel = task.getResult().toObject(UserModel.class);
+            usernameInput.setText(currentUserModel.getUsername());
+            phoneInput.setText(currentUserModel.getPhone());
+        });
+    }
+
+    private void setInProgress(boolean inProgress) {
+        if (inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            updateProfileBtn.setVisibility(View.GONE);
+        }else {
+            progressBar.setVisibility(View.GONE);
+            updateProfileBtn.setVisibility(View.VISIBLE);
+        }
     }
 }
--- app/src/main/java/com/example/chatapk/util/FireBaseUtil.java
+++ app/src/main/java/com/example/chatapk/util/FireBaseUtil.java
@@ -63,7 +63,11 @@ public class FireBaseUtil {
     }
 
     public static String timeStampToString(Timestamp timestamp){
-        return new SimpleDateFormat("HH:MM").format(timestamp.toDate());
+        return new SimpleDateFormat("HH:MM a").format(timestamp.toDate());
+    }
+
+    public static void logout(){
+        FirebaseAuth.getInstance().signOut();
     }
 
 }
